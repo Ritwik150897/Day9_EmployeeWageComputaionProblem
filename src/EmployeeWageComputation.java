04e91e549d90bb7819d9f651e7a0416b051b102f
Ritwik <deshrutu@gmail.com>
2022-05-18 19:51:29 +0530

    UC2 - Daily Wage new

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,33 +1,30 @@
 public class EmployeeWageComputation {
-    double isAbsent = Math.floor(Math.random() * 10) % 3;
+    double isAbsent = Math.floor(Math.random() * 10) % 2;
     int FULL_TIME = 1;
-    int PART_TIME = 2;
     int WAGE_PER_HOUR = 20;
     int empHrs = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program.");
+
         EmployeeWageComputation ewc = new EmployeeWageComputation();
         ewc.isAbsent();
         ewc.dailyWageCalculation();
     }
+
     void isAbsent(){
         if(isAbsent == 1){
             System.out.println("Employee is Present.");
-        } else if (isAbsent == 2) {
-            System.out.println("Employee is Present Part-Time.");
-        } else {
+        }else {
             System.out.println("Employee is Absent.");
         }
     }
+
     void dailyWageCalculation(){
         if(isAbsent == 1){
             empHrs = 8;
             System.out.println("Employee Daily Wage is: "+ empHrs * WAGE_PER_HOUR);
-        } else if (isAbsent == 2) {
-            empHrs = 4;
-            System.out.println("Employee Daily Wage is: "+ empHrs * WAGE_PER_HOUR);
-        } else {
+        }else {
             System.out.println("Employee Daily Wage is: "+ empHrs * WAGE_PER_HOUR);
         }
     }
