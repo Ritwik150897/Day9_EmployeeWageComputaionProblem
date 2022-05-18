b4232caebd43a2aaa98391a350979ff4224a3152
Ritwik <deshrutu@gmail.com>
2022-05-18 19:59:38 +0530

    UC5 - Monthly Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,19 +1,18 @@
 public class EmployeeWageComputation {
-    double isAbsent = Math.floor(Math.random() * 10) % 3;
-    int FULL_TIME = 1;
-    int PART_TIME = 2;
-    int WAGE_PER_HOUR = 20;
-    int empHrs = 0;
+    int FULL_TIME = 1, PART_TIME = 2, WAGE_PER_HOUR = 20, TOTAL_WORKING_DAYS = 20;
+    int empHrs = 0, dailyWage = 0, totalWage = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program.");
         System.out.println("---------------------------------------------");
 
         EmployeeWageComputation ewc = new EmployeeWageComputation();
-        ewc.empAttendance();
+        ewc.monthlyWage();
+        ewc.totalMonthlyWage();
     }
 
     void isAbsent(){
+        double isAbsent = Math.floor(Math.random() * 10) % 3;
         if(isAbsent == FULL_TIME){
             System.out.println("Employee is Present.");
         } else if (isAbsent == PART_TIME) {
@@ -22,7 +21,9 @@ public class EmployeeWageComputation {
             System.out.println("Employee is Absent.");
         }
     }
+
     void dailyWageCalculation(){
+        double isAbsent = Math.floor(Math.random() * 10) % 3;
         if(isAbsent == 1){
             empHrs = 8;
             System.out.println("Employee Daily Wage is: " + empHrs * WAGE_PER_HOUR);
@@ -35,23 +36,60 @@ public class EmployeeWageComputation {
     }
 
     void empAttendance(){
+        double isAbsent = Math.floor(Math.random() * 10) % 3;
         switch ((int) isAbsent){
             case 1:
                 empHrs = 8;
                 System.out.println("Employee is Present for Full-Time.");
                 System.out.println("Employee Daily Wage is: " + empHrs * WAGE_PER_HOUR);
                 break;
-
             case 2:
                 empHrs = 4;
                 System.out.println("Employee is Present for Part-time.");
                 System.out.println("Employee Daily Wage is: " + empHrs * WAGE_PER_HOUR);
                 break;
-
             default:
                 System.out.println("Employee is Absent.");
                 System.out.println("Employee Daily Wage is: " + empHrs * WAGE_PER_HOUR);
                 break;
         }
+    }
+
+    void monthlyWage(){
+        for(int i = 1; i <= TOTAL_WORKING_DAYS; i++){
+            double isAbsent = Math.floor(Math.random() * 10) % 3;
+            switch ((int) isAbsent){
+                case 1:
+                    empHrs = 8;
+                    dailyWage = empHrs * WAGE_PER_HOUR;
+                    System.out.println("Day: " + i);
+                    System.out.println("Employee is Present for Full-Time.");
+                    System.out.println("Employee Daily Wage is: " + dailyWage);
+                    System.out.println("-------------------------");
+                    break;
+
+                case 2:
+                    empHrs = 4;
+                    dailyWage = empHrs * WAGE_PER_HOUR;
+                    System.out.println("Day: " + i);
+                    System.out.println("Employee is Present for Part-time.");
+                    System.out.println("Employee Daily Wage is: " + dailyWage);
+                    System.out.println("-------------------------");
+                    break;
+
+                default:
+                    dailyWage = empHrs * WAGE_PER_HOUR;
+                    System.out.println("Day: " + i);
+                    System.out.println("Employee is Absent.");
+                    System.out.println("Employee Daily Wage is: " + dailyWage);
+                    System.out.println("-------------------------");
+                    break;
+            }
+            totalWage += dailyWage;
+        }
+    }
+
+    void totalMonthlyWage(){
+        System.out.println("Total Monthly Wage: " + totalWage);
     }
 }
